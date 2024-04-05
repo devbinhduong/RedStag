20047efd2e98f935475981a7d2fef78a76a4777f
Le Nguyen Quang Minh <quangminh2170@gmail.com>
2024-04-05 18:00:49 +0700

    Done Function Product Block by List IDs

--- assets/js/theme/bspoq/bspoqGlobal.js
+++ assets/js/theme/bspoq/bspoqGlobal.js
@@ -1,3 +1,4 @@
+import utils from '@bigcommerce/stencil-utils';
 import quickShop from './quickShop';
 import ajaxAddToCart from './ajaxAddToCart';
 
@@ -21,6 +22,10 @@ export default function(context) {
             quickShop(context);
             openMenuMobileEffect();
             toogleFooterMobile();
+
+            if ($('#list-product-detail-block').length > 0) {
+                homeProductsListID();
+            }
         }
     }
 
@@ -310,5 +315,186 @@ export default function(context) {
                 $thisFooterInfo_list.slideUp(400);
             }
         });
+    }
+
+    /* Custom Product Id */
+    function homeProductsListID() {
+        var $options;
+
+        $(document).ready(function () {
+            const tScroll = $(this).scrollTop();
+
+            homeListId(tScroll);
+        });
+
+        function homeListId(tScroll) {
+            if ($('.productsByListId[data-list-id]').length > 0) {
+                $('.productsByListId[data-list-id]').each((index, element) => {
+                    let thisOffetTop =
+                        $(element).offset().top - screen.height * 1.5;
+
+                    if (!$(element).hasClass('is-loaded')) {
+                        $(element).addClass('is-loaded');
+
+                        var $prodWrapId = $(element).attr('id'),
+                            $wrap,
+                            $listId = $(element).data('list-id');
+                        var homeProColumn = $(element)
+                            .parents('.product-block')
+                            .data('columns');
+                        var dots = $(element)
+                            .parents('.product-block')
+                            .data('dots');
+                        var limit = $(element)
+                            .parents('.product-block')
+                            .data('limit');
+
+                        if ($(element).find('.productCarouselCustom').length > 0) {
+                            $wrap = $(element).find('.productCarouselCustom');
+                            $options = {
+                                template:
+                                    'bspoq/products/custom-product-block',
+                            };
+                        }
+
+                        if (
+                            !$(
+                                '#product-by-list-' +
+                                    $prodWrapId +
+                                    ' .productCarouselCustom .productCarousel-slide'
+                            ).length
+                        ) {
+                            if ($listId.length > 1) {
+                                loadListID(
+                                    $listId,
+                                    $options,
+                                    $wrap,
+                                    homeProColumn,
+                                    dots,
+                                    limit
+                                );
+                            } else {
+                                loadListID(
+                                    $($listId),
+                                    $options,
+                                    $wrap,
+                                    homeProColumn,
+                                    dots,
+                                    limit
+                                );
+                            }
+                        }
+                    }
+                });
+            }
+        }
+
+        function loadListID(id, options, wrap, homeProColumn, dots, limit) {
+            if (id.length <= 1) {
+                var arr = id;
+            } else {
+                var arr = id.split(',');
+            }
+
+            if (id.length > homeProColumn) {
+                var list = arr.slice(0, parseInt(limit));
+            } else {
+                var list = arr;
+            }
+
+            var num = 0;
+
+            for (var i = 0; i <= list.length; i++) {
+                var $prodId = list[i];
+
+                if ($prodId != undefined) {
+                    utils.api.product.getById(
+                        $prodId,
+                        options,
+                        (err, response) => {
+                            let hasProd = $(response)
+                                .find('.card')
+                                .data('product-id');
+                            if (
+                                hasProd != undefined &&
+                                hasProd != '' &&
+                                hasProd != null &&
+                                !$(response).find('.page-content--err').length
+                            ) {
+                                if (wrap.hasClass('slick-slider')) {
+                                    wrap.slick('unslick');
+                                    wrap.append(response);
+                                } else {
+                                    wrap.append(response);
+                                }
+                            }
+
+                            num++;
+
+                            if (num == list.length) {
+                                wrap.parents('.productsByListId[data-list-id]')
+                                    .find('.custom_productLoading')
+                                    .remove();
+                                wrap.parents(
+                                    '.productsByListId[data-list-id]'
+                                ).addClass('show');
+                                if (wrap.hasClass('productCarouselCustom')) {
+                                    slickCarouselListId(
+                                        wrap,
+                                        homeProColumn,
+                                        dots
+                                    );
+                                }
+                                return;
+                            }
+                        }
+                    );
+                }
+            }
+        }
+
+        function slickCarouselListId($wrap, homeProColumn, dots) {
+            $wrap.slick({
+                infinite: false,
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                dots: true,
+                arrows: false,
+                mobileFirst: true,
+                nextArrow:
+                    "<svg class='slick-next slick-arrow'><use xlink:href='#slick-arrow-next'></use></svg>",
+                prevArrow:
+                    "<svg class='slick-prev slick-arrow'><use xlink:href='#slick-arrow-prev'></use></svg>",
+                responsive: [
+                    {
+                        breakpoint: 1280,
+                        settings: {
+                            slidesToShow: homeProColumn,
+                            slidesToScroll: homeProColumn,
+                            arrows: true,
+                            dots: dots,
+                        },
+                    },
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: homeProColumn - 2,
+                            slidesToScroll: homeProColumn - 2,
+                            arrows: true,
+                            dots: dots,
+                        },
+                    },
+                    {
+                        breakpoint: 767,
+                        settings: {
+                            slidesToShow: homeProColumn - 2,
+                            slidesToScroll: homeProColumn - 2,
+                            arrows: false,
+                            dots: true,
+                        },
+                    },
+                ],
+            });
+        }
     }
 } 
